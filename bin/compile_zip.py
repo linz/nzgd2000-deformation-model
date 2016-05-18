d021e3ac2e373d73d08e41aef8a21c12ba14a1d4
Chris Crook <ccrook@linz.govt.nz>
2016-05-19 11:21:45 +1200

    Fixing building of release zip file

--- bin/compile_zip.py
+++ bin/compile_zip.py
@@ -10,7 +10,7 @@ from StringIO import StringIO
 from zipfile import ZipFile
 
 
-def get_source_modules(repo,toolsdir):
+def get_source_modules(repo,toolsdir,files=None):
     print "Retrieving",repo
     url='https://github.com/linz/'+repo+'/zipball/master'
     r=requests.get(url)
@@ -20,34 +20,48 @@ def get_source_modules(repo,toolsdir):
             continue
         m = re.match(r'^[\w\-]*(\/LINZ\/.+)$',n)
         if m:
-            print "Extracting "+m.group(1)
-            tgtfile=toolsdir+m.group(1)
+            filepath=m.group(1)
+            print "Extracting "+filepath
+            tgtfile=toolsdir+filepath
             path=os.path.dirname(tgtfile)
             if not os.path.isdir(path):
                 os.makedirs(path)
+
+            filename=os.path.basename(filepath)
+            # Replace __init__.py with empty file to avoid package resources
+            if filename == '__init__.py':
+                with open(tgtfile,'wb') as tf:
+                    pass
+                continue
+            # If selecting files then skip unmatched files
+            if files and filename not in files:
+                print "Omitting",filename
+                continue
             with open(tgtfile,'wb') as tf:
                 tf.write(z.read(n))
 
 def create_python_program(module,toolsdir):
     if not os.path.exists(toolsdir+'/LINZ/DeformationModel/'+module+'.py'):
         raise RuntimeError('Cannot find module {0} in LINZ.DeformationModel'.format(module))
-    src='''#!/bin/python
+    src='''#!/usr/bin/python
         import sys
         import os.path
-        sys.path.insert(0,os.path.dirname(__file__))
+        sys.path.insert(1,os.path.dirname(os.path.abspath(__file__)))
         from LINZ.DeformationModel import {module}
         {module}.main()
         '''
     src=src.replace('{module}',module)
     src=re.sub(r'^\s+','',src,flags=re.MULTILINE)
-    with open(toolsdir+'/'+module+'.py','w') as pyf:
+    progfile=toolsdir+'/'+module+'.py'
+    with open(progfile,'w') as pyf:
         pyf.write(src)
+    os.chmod(progfile,0755)
 
 def build_tools( toolsdir ):
     if not os.path.isdir(toolsdir):
         os.makedirs(toolsdir)
     get_source_modules('python-linz-deformationmodel',toolsdir)
-    get_source_modules('python-linz-geodetic',toolsdir)
+    get_source_modules('python-linz-geodetic',toolsdir,['Ellipsoid.py','ITRF.py'])
     create_python_program('CalcDeformation',toolsdir)
     create_python_program('ITRF_NZGD2000',toolsdir)
     create_python_program('NZGD2000_conversion_grid',toolsdir)
